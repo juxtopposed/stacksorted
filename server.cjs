502b481e0f2c5a8a82baec8ea9a15ff25df94c7f
3top1a <3top1a.official@gmail.com>
2023-07-25 12:28:26 +0200

    Added compression

--- server.cjs
+++ server.cjs
@@ -1,9 +1,11 @@
 const express = require('express');
+const compression = require('compression')
 const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 50985;
 
+app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {

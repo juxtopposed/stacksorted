b7f021e275c5cb4b9f947d523ca020cacdbf257f
heyanik <anikdaspartha58@gmail.com>
2023-09-25 00:51:02 +0600

    change footer style

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,8 +1,53 @@
 <main class="footer-content">
-  <p class="footer-col1"><span style="font-size: 16px;">What is Stack Sorted?</span><br>It's an open-source project that categorizes websites, not by general design or topic but specific elements. All screenshots, designs, and codes are © of their respective owners.</p>
-  <p>Made by <a href="https://twitter.com/juxtopposed" target="_blank" class="link">Juxtopposed</a></p>
-  <p><a href="https://github.com/juxtopposed/stacksorted/blob/main/src/database.js" target="_blank" class="link">Add your own sites</a></p>
-  <p><a href="https://github.com/juxtopposed/stacksorted" target="_blank" class="link">Edit this on GitHub</a></p>
-  <p><a href="https://ko-fi.com/juxtopposedme" target="_blank" class="link">Support this project</a></p>
-  <p><a href="https://twitter.com/intent/tweet?text=Check%20out%20Stack%20Sorted:%20stacksorted.com%20-%20You%20can%20find%20inspiration%20for%20design%20elements." target="_blank" class="link">Tweet about this</a></p>
+  <div class="footer-container">
+    <div>
+      <p class="footer-col1">
+        <span style="font-size: 16px;">What is Stack Sorted?</span><br /><span
+          style="font-size: 12px;"
+          >It's an open-source project that categorizes websites, not by general
+          design or topic but specific elements. All screenshots, designs, and
+          codes are © of their respective owners.</span
+        >
+      </p>
+    </div>
+    <div>
+      <p>
+        <a
+          href="https://github.com/juxtopposed/stacksorted/blob/main/src/database.js"
+          target="_blank"
+          class="link">Add your own sites</a
+        >
+      </p>
+      <p>
+        <a
+          href="https://github.com/juxtopposed/stacksorted"
+          target="_blank"
+          class="link">Edit this on GitHub</a
+        >
+      </p>
+    </div>
+    <div>
+      <p>
+        <a href="https://ko-fi.com/juxtopposedme" target="_blank" class="link"
+          >Support this project</a
+        >
+      </p>
+      <p>
+        <a
+          href="https://twitter.com/intent/tweet?text=Check%20out%20Stack%20Sorted:%20stacksorted.com%20-%20You%20can%20find%20inspiration%20for%20design%20elements."
+          target="_blank"
+          class="link">Tweet about this</a
+        >
+      </p>
+    </div>
+  </div>
+  <div class="m-tag">
+    <p>
+      Made by <a
+        href="https://twitter.com/juxtopposed"
+        target="_blank"
+        class="link">Juxtopposed</a
+      >
+    </p>
+  </div>
 </main>
